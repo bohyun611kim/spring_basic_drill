f0ad1f86c58e69d6e5994dfceecdbb2ca3eda818
bonggue <bohyun611.kim@gmail.com>
2023-04-07 11:40:42 +0900

    상태코드 추가

--- src/main/java/com/fastcampus/ch2/exception/GlobalCatcher.java
+++ src/main/java/com/fastcampus/ch2/exception/GlobalCatcher.java
@@ -1,13 +1,16 @@
 package com.fastcampus.ch2.exception;
 
 import java.io.FileNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice("com.fastcampus.ch2") // 지정된 공통예외처리 가능
 public class GlobalCatcher {
 
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 200 -> 40X, 50X 변환하고자 
     @ExceptionHandler({NullPointerException.class, FileNotFoundException.class })
     public String catcher2(Exception ex, Model model) {
         model.addAttribute("ex", ex);
