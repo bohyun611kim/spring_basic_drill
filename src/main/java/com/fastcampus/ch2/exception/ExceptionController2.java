1a48908962ffc0120150dfc4bbce5f9280cb7bd9
bonggue <bohyun611.kim@gmail.com>
2023-04-07 11:07:34 +0900

    global catcher 예외처리 공통처리

---
+++ src/main/java/com/fastcampus/ch2/exception/ExceptionController2.java
@@ -0,0 +1,25 @@
+package com.fastcampus.ch2.exception;
+
+
+import java.io.FileNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ExceptionController2 {
+
+    // Global catcher 예외처리 추가
+
+    @RequestMapping("/ex3")
+    public String main() throws Exception {
+        throw new Exception("ex 예외가 발생하였습니다.");
+    }
+
+    @RequestMapping("/ex4")
+    public String main2() throws Exception {
+        throw new FileNotFoundException("ex 예외가 발생하였습니다.");
+    }
+
+}
--- src/main/java/com/fastcampus/ch2/exception/GlobalCatcher.java
+++ src/main/java/com/fastcampus/ch2/exception/GlobalCatcher.java
@@ -1,14 +1,12 @@
 package com.fastcampus.ch2.exception;
 
-
 import java.io.FileNotFoundException;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-public class ExceptionController {
+@ControllerAdvice
+public class GlobalCatcher {
 
     @ExceptionHandler({NullPointerException.class, FileNotFoundException.class })
     public String catcher2(Exception ex, Model model) {
@@ -19,16 +17,6 @@ public class ExceptionController {
     @ExceptionHandler(Exception.class)
     public String catcher(Exception ex) {
         return "error";
-    }
-
-    @RequestMapping("/ex")
-    public String main() throws Exception {
-        throw new Exception("ex 예외가 발생하였습니다.");
-    }
-
-    @RequestMapping("/ex2")
-    public String main2() throws Exception {
-        throw new FileNotFoundException("ex 예외가 발생하였습니다."); // 예외 조상이 처리한다.
     }
 
 }
