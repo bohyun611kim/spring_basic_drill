638e79660120518f347fab2c930735e94b3011a2
bonggue <bohyun611.kim@gmail.com>
2023-04-07 10:42:55 +0900

    예외처리 메소드별 중복상황

--- src/main/java/com/fastcampus/ch2/exception/ExceptionController.java
+++ src/main/java/com/fastcampus/ch2/exception/ExceptionController.java
@@ -2,18 +2,30 @@ package com.fastcampus.ch2.exception;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ExceptionController {
-
+    
     @RequestMapping("/ex")
-    public void main() throws Exception {
+    public String main() throws Exception {
 
         try {
             throw new Exception("예외가 발생하였습니다.");
         } catch (Exception e) {
-            e.getMessage();
+            return "error";
+        }
+
+    }
+
+    @RequestMapping("/ex2")
+    public String main2() throws Exception {
+
+        try {
+            throw new Exception("예외가 발생하였습니다.");
+        } catch (Exception e) {
+            return "error";
         }
 
     }
