28d31263f68b46e2875c00d38c92b91443167ddb
bonggue <bohyun611.kim@gmail.com>
2023-04-07 13:23:53 +0900

    상태코드 추가  예시 추가

--- src/main/java/com/fastcampus/ch2/exception/ExceptionController.java
+++ src/main/java/com/fastcampus/ch2/exception/ExceptionController.java
@@ -2,10 +2,12 @@ package com.fastcampus.ch2.exception;
 
 
 import java.io.FileNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class ExceptionController {
@@ -16,8 +18,12 @@ public class ExceptionController {
         return "error";
     }
 
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 200 -> 500
     @ExceptionHandler(Exception.class)
-    public String catcher(Exception ex) {
+    public String catcher(Exception ex, Model model) {
+        System.out.println("--- catcher");
+        // status code : 200 변경되어야 한다.
+        //model.addAttribute("ex", ex);
         return "error";
     }
 
