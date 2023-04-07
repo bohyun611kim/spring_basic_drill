c976a75fd1d11762576f169a933c8f7801868643
bonggue <bohyun611.kim@gmail.com>
2023-04-07 10:54:08 +0900

    예외처리 정보 error 노출

--- src/main/java/com/fastcampus/ch2/exception/ExceptionController.java
+++ src/main/java/com/fastcampus/ch2/exception/ExceptionController.java
@@ -2,32 +2,32 @@ package com.fastcampus.ch2.exception;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ExceptionController {
-    
+
+    @ExceptionHandler(NullPointerException.class)
+    public String catcher2(Exception ex, Model model) {
+        model.addAttribute("ex", ex);
+        return "error";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String catcher(Exception ex) {
+        return "error";
+    }
+
     @RequestMapping("/ex")
     public String main() throws Exception {
-
-        try {
-            throw new Exception("예외가 발생하였습니다.");
-        } catch (Exception e) {
-            return "error";
-        }
-
+        throw new Exception("ex 예외가 발생하였습니다.");
     }
 
     @RequestMapping("/ex2")
     public String main2() throws Exception {
-
-        try {
-            throw new Exception("예외가 발생하였습니다.");
-        } catch (Exception e) {
-            return "error";
-        }
-
+        throw new NullPointerException("ex 예외가 발생하였습니다.");
     }
 
 }
